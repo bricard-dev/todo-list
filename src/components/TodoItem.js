2d9dfc1e2ffaa1978a75e5e877f37a81592d6994
Bastien Ricard <bastien.ricard-dev@outlook.com>
2023-02-01 00:16:39 +0100

    feat: add select a todo
    Add the selection of a todo among the todo list.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,45 @@
-function TodoItem({ todo, deleteTodo, toggleTodo, toggleTodoEdit }) {
+function TodoItem({
+  todo,
+  deleteTodo,
+  toggleTodo,
+  toggleTodoEdit,
+  selectTodo,
+}) {
   return (
-    <li className="mb-10 d-flex justify-content-center align-items-center p-10">
+    <li
+      onClick={selectTodo}
+      className={`mb-10 d-flex justify-content-center align-items-center p-10 ${
+        todo.selected && "selected"
+      }`}
+    >
       <span className="flex-fill">
         {todo.content} {todo.done && "✅"}
       </span>
-      <button className="btn btn-primary mr-15" onClick={toggleTodo}>
+      <button
+        className="btn btn-primary mr-15"
+        onClick={(event) => {
+          event.stopPropagation();
+          toggleTodo();
+        }}
+      >
         Valider
       </button>
-      <button className="btn btn-primary mr-15" onClick={toggleTodoEdit}>
+      <button
+        className="btn btn-primary mr-15"
+        onClick={(event) => {
+          event.stopPropagation();
+          toggleTodoEdit();
+        }}
+      >
         Modifier
       </button>
-      <button onClick={deleteTodo} className="btn btn-reverse-primary">
+      <button
+        className="btn btn-reverse-primary"
+        onClick={(event) => {
+          event.stopPropagation();
+          deleteTodo();
+        }}
+      >
         Supprimer
       </button>
     </li>
